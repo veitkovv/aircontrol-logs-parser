f68416200c13e2eb197f53f48c2ed844f3d6a127
veytko-vv <veitko.vv@gmail.com>
2019-08-20 10:56:02 +0500

    update packages npm

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,19 +3,12 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './plugins/router'
 import {store} from './plugins/store/main'
-import DaySpanVuetify from 'dayspan-vuetify'
 
 import 'vuetify/dist/vuetify.min.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
-import 'dayspan-vuetify/dist/lib/dayspan-vuetify.min.css'
 
 Vue.config.productionTip = false
 
-Vue.use(DaySpanVuetify, {
-    methods: {
-        getDefaultEventColor: () => '#1976d2'
-    }
-});
 
 new Vue({
     vuetify,

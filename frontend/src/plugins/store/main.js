dfdf3f6d2d5a10cce2589859f4506b59887d0579
veytko-vv <veitko.vv@gmail.com>
2019-09-09 15:23:31 +0500

    del endpoint env

--- frontend/src/plugins/store/main.js
+++ frontend/src/plugins/store/main.js
@@ -3,7 +3,7 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
-const apiEndpoint = env.process.get.VUE_APP_API_ENDPOINT || "api.yr-cinegylog-parser.yaregion.local";
+const apiEndpoint = "api.yr-cinegylog-parser.yaregion.local";
 
 const state = {
     events: [],

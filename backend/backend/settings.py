fad51c51ce898998acc519b9ef13a183fe47f7e5
veytko-vv <veitko.vv@gmail.com>
2019-10-01 11:43:58 +0500

    pagination

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -118,7 +118,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 5000,
+    'PAGE_SIZE': 25000,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
